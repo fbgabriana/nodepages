0e3eb7de1e55f36b033a2085b30cfd817d061e1d
fbgabriana <fbgabriana@gmail.com>
2022-12-05 22:51:03 +0800

    Random updates

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 const hostname = "localhost";
 
 const server = http.createServer((req, res) => {
